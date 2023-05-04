38d7cb42bc08232bd734c7dfb40070d17e994e3e
DPM <dylan.phillip.meyers@gmail.com>
2023-05-04 21:23:08 +0900

    ascii

---
+++ ascii.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+/*Create ASCII art*/
+      Console.WriteLine(" .-."); 
+      Console.WriteLine("(o o)"); 
+      Console.WriteLine("| 0 |");  
+      Console.WriteLine("|   |"); 
+      Console.WriteLine("'~~~'");  
+      Console.WriteLine("Minions We Have @ Home");
+      Console.WriteLine("c_    ___"); 
+      Console.WriteLine("  \\\\__|__|__"); 
+      Console.WriteLine("     \\( o_o)"); 
+      Console.WriteLine("       > ~  >"); 
+      Console.WriteLine("    /    / \\\\"); 
+      Console.WriteLine("   /    /   \\\\"); 
+      Console.WriteLine("   \\   )    c/"); 
+      Console.WriteLine("   /  /"); 
+      Console.WriteLine("  /  /|"); 
+      Console.WriteLine(" (  ( \\");
+      Console.WriteLine(" |  |  \\"); 
+      Console.WriteLine(" | / \\  )"); 
+      Console.WriteLine(" | | )  |"); 
+      Console.WriteLine(" / )(_/"); 
+      Console.WriteLine("(_ /");   
+     }
+  }
+}
