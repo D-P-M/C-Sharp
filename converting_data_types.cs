38ecf092eca14bc1b10a24795ddcf7b9656df125
DPM <dylan.phillip.meyers@gmail.com>
2023-05-05 12:48:24 +0900

    converting data types

---
+++ converting_data_types.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace FavoriteNumber
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Ask user for fave number
+      Console.Write("Enter your favorite number!: ");
+      // Turn that answer into an int
+
+      // int faveNumber = 
+      // Console.ReadLine();
+
+      // int faveNumber = (int)Console.ReadLine();
+
+      int faveNumber = Convert.ToInt32(Console.ReadLine());
+      Console.Write("Your fav number is ");
+      Console.Write(faveNumber);
+
+
+    }
+  }
+}
