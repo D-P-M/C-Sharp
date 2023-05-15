124491522e781049962c81d2981288a5f4bf3e2c
DPM <dylan.phillip.meyers@gmail.com>
2023-05-15 18:42:17 +0900

    loop rules

--- loop_rules.cs
+++ loop_rules.cs
@@ -8,4 +8,24 @@ but then stop if a condition is met.
 for loops are best if you want something to run a specific number of times, 
 rather than given a certain condition.
 
-foreach loops are the best way to loop over an array, or any other collection. */
+foreach loops are the best way to loop over an array, or any other collection.
+
+
+
+A loop is a structure in programming where the instructions are written once, 
+but a computer can execute them multiple times
+
+Each execution of those instructions is called an iteration
+
+while loops repeat until a condition changes
+
+do...while loops execute once, and then repeat until a condition changes
+
+for loops repeat for a specified number of times
+
+foreach loops repeat for each item in a collection
+
+jump statements, like break, continue, and return are used to add additional control flow to loops
+ */
+
+
